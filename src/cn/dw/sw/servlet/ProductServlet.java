6c9a553bc2e9a3e19a209b08af89d163acaf1b3b
aubrey <676719570@qq.com>
2018-09-04 14:29:35 +0800

    完善servlet的新增功能

--- src/cn/dw/sw/servlet/ProductServlet.java
+++ src/cn/dw/sw/servlet/ProductServlet.java
@@ -12,6 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.dw.sw.model.Product;
+import cn.dw.sw.service.ProductService;
+import cn.dw.sw.service.impl.ProductServiceImpl;
+
 /**
  * @author aubrey
  * @date  上午11:32:16
@@ -20,13 +24,21 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/product")
 public class ProductServlet  extends HttpServlet{
 
+	private ProductService productService  = new ProductServiceImpl();
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String name = req.getParameter("name");
-		String age = req.getParameter("age");
+		String type = req.getParameter("type");
+		
+		Product product = new Product();
+		product.setName(name);
+		product.setType(type);
+		productService.add(product);
+		
 		PrintWriter print = resp.getWriter();
-		print.write(name);
-		print.write(age);
+		print.write("success");
 		
 	}
 
